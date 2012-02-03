a6712ebc521f0450e1a3ed8cc09c5a9ba8b0318f
Darko Mikulic <dmikulicwr@gmail.com>
2012-02-03 17:31:55 +0100

    Delimiter fix

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A Very Gentle Introduction to Relational and Functional Programming"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.macro "0.1.1"]
-                 [org.clojure/core.logic "0.6.5"])
+                 [org.clojure/core.logic "0.6.5"]])
